8f21db4dbd17c408ad050baa0d42838df845ff86
gheaputrinashirah-dotcom <gheaputrinashirah@gmail.com>
2025-10-20 21:58:07 +0700

    Update tagheajudul5.cpp

--- tagheajudul5.cpp
+++ tagheajudul5.cpp
@@ -29,38 +29,12 @@ void inorder(Node* root) {
     inorder(root->right);
 }
 
-void preorder(Node* root) {
-    if (!root) return;
-    cout << root->npm << " ";
-    preorder(root->left);
-    preorder(root->right);
-}
-
-void postorder(Node* root) {
-    if (!root) return;
-    postorder(root->left);
-    postorder(root->right);
-    cout << root->npm << " ";
-}
-
-int findMin(Node* root) {
-    if (!root) return -1;
-    while (root->left) root = root->left;
-    return root->npm;
-}
-
-int findMax(Node* root) {
-    if (!root) return -1;
-    while (root->right) root = root->right;
-    return root->npm;
-}
-
 int countNodes(Node* root) {
     if (!root) return 0;
     return 1 + countNodes(root->left) + countNodes(root->right);
 }
 
-long long sumNodes(Node* root) {
+int sumNodes(Node* root) {
     if (!root) return 0;
     return root->npm + sumNodes(root->left) + sumNodes(root->right);
 }
@@ -74,13 +48,9 @@ int main() {
         cout << "1. Insert NPM\n";
         cout << "2. Search NPM\n";
         cout << "3. Inorder\n";
-        cout << "4. Preorder\n";
-        cout << "5. Postorder\n";
-        cout << "6. NPM terkecil\n";
-        cout << "7. NPM terbesar\n";
-        cout << "8. Jumlah data NPM\n";
-        cout << "9. Total penjumlahan semua NPM\n";
-        cout << "10. Keluar\n";
+        cout << "4. Jumlah data NPM\n";
+        cout << "5. Total penjumlahan semua NPM\n";
+        cout << "6. Keluar\n";
         cout << "Pilih menu: ";
         cin >> pilih;
 
@@ -88,43 +58,20 @@ int main() {
             cout << "Masukkan NPM: ";
             cin >> npm;
             root = insertNode(root, npm);
-            cout << "Data NPM berhasil ditambahkan!\n";
-        } 
-        else if (pilih == 2) {
+        } else if (pilih == 2) {
             cout << "Masukkan NPM yang dicari: ";
             cin >> npm;
             cout << (searchNode(root, npm) ? "NPM ditemukan!\n" : "NPM tidak ditemukan!\n");
-        } 
-        else if (pilih == 3) {
+        } else if (pilih == 3) {
             cout << "Traversal Inorder: ";
             inorder(root);
             cout << "\n";
-        } 
-        else if (pilih == 4) {
-            cout << "Traversal Preorder: ";
-            preorder(root);
-            cout << "\n";
-        } 
-        else if (pilih == 5) {
-            cout << "Traversal Postorder: ";
-            postorder(root);
-            cout << "\n";
-        } 
-        else if (pilih == 6) {
-            cout << "NPM terkecil: " << findMin(root) << "\n";
-        } 
-        else if (pilih == 7) {
-            cout << "NPM terbesar: " << findMax(root) << "\n";
-        } 
-        else if (pilih == 8) {
+        }  else if (pilih == 8) {
             cout << "Jumlah data NPM: " << countNodes(root) << "\n";
-        } 
-        else if (pilih == 9) {
+        } else if (pilih == 9) {
             cout << "Total semua NPM: " << sumNodes(root) << "\n";
         }
-
     } while (pilih != 10);
-
-    cout << "Program selesai.\n";
+    
     return 0;
 }
